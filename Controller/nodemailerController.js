ff37b22dcb7d14fc68e6b6482a2ad99ee075016c
unknown <hevarsharma@gmail.com>
2022-07-06 15:07:22 +0530

    sending png files

--- Controller/nodemailerController.js
+++ Controller/nodemailerController.js
@@ -1,7 +1,6 @@
 const nodemailer = require('nodemailer');
 
-const { jsPDF } = require("jspdf");
-const pdf = new jsPDF();
+const path = require('path');
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -15,8 +14,7 @@ exports.sendMails = async (req, res) => {
 
     try {
         const user = req.body;
-        let userPdfPath = toPdf(user.image, user.email);
-        console.log(userPdfPath);
+        let userPdfPath = toPng(user.image, user.email);
 
         mailOption = {
             from: process.env.EMAIL,
@@ -25,9 +23,8 @@ exports.sendMails = async (req, res) => {
             text: "Here is yours Access card.",
             attachments: [
                 {
-                    filename: 'Access-Card',
-                    path: userPdfPath,
-                    contentType: "application/pdf",
+                    filename: 'Access-Card.png',
+                    path: path.join(userPdfPath),
                 },
             ],
         }
@@ -46,20 +43,13 @@ exports.sendMails = async (req, res) => {
 
 }    //function for sending the mail via api/sendMails
 
-function toPdf(imageUrl, userMail) {
+function toPng(imageUrl, userMail) {
 
     let base64image = imageUrl.split(';base64,').pop();
 
-    const imgProps = pdf.getImageProperties(base64image);
-
-    const pdfWidth = pdf.internal.pageSize.getWidth();
-
-    const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-
-    pdf.addImage(base64image, "JPEG", 0, 0, pdfWidth, pdfHeight / 1.13);
-
-    pdf.save(`./pdfFiles/${userMail}.pdf`);
-
-    return `./pdfFiles/${userMail}.pdf`
+    require("fs").writeFile(  `./pdfFiles/${userMail}.png`, base64image,{encoding:'base64'}, function (err) {
+        console.log(err);
+    });
+    return `./pdfFiles/${userMail}.png`
 
 }  //function for converting base64 to pdf file.
